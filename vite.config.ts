4fbc8c96ec7539a53e2d11ad90481d2b93036791
Mael <mael.constantin@gmail.com>
2026-03-13 17:51:34 +0100

    Modification serveur dev vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    host: "0.0.0.0",
+    port: 5173,
+    strictPort: true,
+    allowedHosts: ["nas.maelconstantin.fr"],
+    hmr: {
+      protocol: "wss",
+      host: "nas.maelconstantin.fr",
+      clientPort: 9900,
+    },
+  },
 });
