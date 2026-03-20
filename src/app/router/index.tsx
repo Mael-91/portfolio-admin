7fc25d7904c9feaad4df5faf90585447e11efab9
Mael <mael.constantin@gmail.com>
2026-03-20 23:07:22 +0100

    routes rgpd

--- src/app/router/index.tsx
+++ src/app/router/index.tsx
@@ -35,7 +35,7 @@ export const router = createBrowserRouter([
         element: <MessageDetailPage />,
       },
       {
-        path: "/settings",
+        path: "/settings/rgpd",
         element: <SettingsPage />,
       },
       {
