8eb070f4a85a9b140587e3864b63716ca0cf9fbc
Mael <mael.constantin@gmail.com>
2026-04-01 12:54:18 +0200

    Update Button.tsx

--- src/app/components/ui/Button.tsx
+++ src/app/components/ui/Button.tsx
@@ -41,7 +41,7 @@ export function Button({
         variant === "dangerSoft" &&
           "bg-red-500/10 text-red-400 hover:bg-red-500/20 hover:text-red-300",
         variant === "icon" &&
-          "relative bg-transparent border-none hover:bg-white/[0.1] rounded-lg",
+          "relative bg-transparent border-none rounded-lg",
 
         className,
 
